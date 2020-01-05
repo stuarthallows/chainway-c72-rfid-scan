aff5030f4fa9b00872cc450218a1b7db27e7e22f
Stuart Hallows <stuart.hallows@gmail.com>
2020-01-05 18:20:30 +1030

    Add Xamarin project

---
+++ C72Scan/C72Scan/C72Scan/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using C72Scan.Models;
+
+namespace C72Scan.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
