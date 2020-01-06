201de986c911e7c261aa4d3264f5582041f7f1a1
Stuart Hallows <stuart.hallows@gmail.com>
2020-01-07 09:09:49 +1030

    support running on VM without BT

--- C72Scan/C72Scan/C72Scan.Android/Services/RfidService.cs
+++ C72Scan/C72Scan/C72Scan.Android/Services/RfidService.cs
@@ -8,36 +8,59 @@ namespace C72Scan.Droid.Services
 {
     public class RfidService : IRfidService
     {
-        public RFIDWithUHF uhfAPI;
+#if NO_BLUETOOTH
+#else
+        private RFIDWithUHF uhfAPI;
 
         public RfidService()
         {
             uhfAPI = RFIDWithUHF.Instance;
         }
+#endif
 
         public bool StopInventory()
         {
+#if NO_BLUETOOTH
+            return true;
+#else
             return uhfAPI.StopInventory();
+#endif
         }
 
         public bool Init()
         {
+#if NO_BLUETOOTH
+            return true;
+#else
             return uhfAPI.Init();
+#endif
         }
 
         public bool Free()
         {
+#if NO_BLUETOOTH
+            return true;
+#else
             return uhfAPI.Free();
+#endif
         }
 
         public string InventorySingleTag()
         {
+#if NO_BLUETOOTH
+            return "X9999ASDFZZZZZ";
+#else
             return uhfAPI.InventorySingleTag();
+#endif
         }
 
         public string ConvertUiiToEpc(string uii)
         {
+#if NO_BLUETOOTH
+            return "X9999ASDFZZZZZ9999";
+#else
             return uhfAPI.ConvertUiiToEPC(uii);
+#endif
         }
     }
 }
